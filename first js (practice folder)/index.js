74174398e07dc724e01cd4b501855c71098ab5e3
Lavish0007 <patellavish.9336@gmail.com>
2026-01-18 20:40:46 +0530

    new changes

---
+++ first js (practice folder)/index.js
@@ -0,0 +1,26 @@
+// console.log("Hello, World!");
+// console.log("This is a simple JavaScript file.");
+// // alert("Welcome to JavaScript programming!");
+
+// let person = {
+//     name: "Alice",
+//     age: 30,
+//     city: "New York"
+
+// }
+// person['city'] = "Los Angeles";
+// console.log(person.city);
+
+// function greet(name, last="Smith") {
+//     return `Hello, ${name} ${last}!`;
+// }
+
+// console.log(greet(20));
+
+// console.log(a);
+
+// let a = 10;
+
+for(let i=1;i<11;i++){
+    console.log(i)
+}
