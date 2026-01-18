444d9823d6970738a801d7f51d2c088712137437
Lavish0007 <patellavish.9336@gmail.com>
2026-01-18 20:56:25 +0530

    new changes

--- first js (practice folder)/index.js
+++ first js (practice folder)/index.js
@@ -20,7 +20,14 @@
 // console.log(a);
 
 // let a = 10;
-
-for(let i=1;i<11;i++){
-    console.log(i)
-}
+// let a=5;
+// for(let i=1;i<11;i++){
+//      console.log(`5 * ${i} = ${5*i}`)
+// }
+// let a=10;
+// while(a>=1){
+//     console.log(a);
+//     a--;
+// }
+// let a=100;
+// console.log((a*(a+1))/2)
