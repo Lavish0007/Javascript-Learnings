6b0d6c3999a87d4892e8514271d41543b353c97b
Lavish0007 <patellavish.9336@gmail.com>
2026-02-22 17:49:56 +0530

    done

--- first js (practice folder)/objects/first.js
+++ first js (practice folder)/objects/first.js
@@ -1,13 +1,18 @@
+let role="zetta";
+
 obj={
     name:"Lavish",
+    age:21,
     Address:{
         city:"ghaziabad",
         loc:{
             lat:28.6692,
             lon:77.4538
         }
-    }
+    },
+    [role]:"Wo bhi Lavish"
 }
+
 let obj2=JSON.parse(JSON.stringify(obj));
 obj2.Address.city="Delhi";
 // for(let keyy in obj){
